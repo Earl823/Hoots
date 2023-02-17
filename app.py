efb8a1e3ecf7c742e33125dabd7203d66b5961ee
Earl Joseph Bayron <57792991+Earl823@users.noreply.github.com>
2023-02-17 23:13:41 +0800

    adding signup

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def home():
 def login():
 	return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+	return render_template('signUp.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
